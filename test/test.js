72d403ec3055a515507dca27be6eb714d9c86169
Glitch (imdarkdiamond-captcha-gen) <none>
2019-02-03 18:47:35 +0000

    Update Example

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
+// Require Packages
 const Captcha = require('../index.js');
 const fs = require('fs');
 
+// Generate Captcha
 const captcha = new Captcha(150, 50, 5, { fontSize: 55 }); 
 const { canvas, ctx } = captcha.initCanvas();
-
 captcha.drawLine(ctx);
 const text = captcha.drawText(ctx);
+
+// Write Captcha
 fs.writeFile('image.png', canvas.toBuffer(), (err) => {
   if (err) throw err;
   console.log('Generated!');
